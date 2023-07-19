152cc0e0234df999aabc7834fe75926e64fe5f23
Hardik Pratap <hardikpratap188@gmail.com>
2023-07-19 23:24:29 +0530

    init

--- src/SortingTechniques/selectionSort.java
+++ src/SortingTechniques/selectionSort.java
@@ -1,7 +1,39 @@
 package SortingTechniques;
 
 public class selectionSort {
+    //    Basic idea:
+//    1:Loop in array
+//    2:Get MAX number
+//    3:Swap Max number with last index
+
     public static void main(String[] args) {
-        System.out.println("selection sort");
+        System.out.println("This is selection sort");
+        int[] arr={5,4,3,2,1};
+        selection(arr);
+
     }
+
+    public static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last=arr.length-i-1;
+            int maxNum=max(arr,0,last);
+            swap(arr,maxNum,last);
+        }
+    }
+
+    public static int max(int[] arr, int start, int last) {
+        int max=start;
+        for (int j = start; j < last; j++) {
+            if(arr[j]>arr[max])
+                max=j;
+        }
+        return max;
+    }
+    public static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+
 }
