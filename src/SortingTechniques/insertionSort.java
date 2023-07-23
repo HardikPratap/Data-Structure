a49fae8beb3728b17a5b821255b369d81e5e78ed
Hardik Pratap <hardikpratap188@gmail.com>
2023-07-23 23:07:51 +0530

    main file- insertion sort

--- src/SortingTechniques/insertionSort.java
+++ src/SortingTechniques/insertionSort.java
@@ -1,5 +1,16 @@
 package SortingTechniques;
 
-public class insertionSort {
+import java.util.Arrays;
 
+public class insertionSort {
+    public static void main(String[] args) {
+        System.out.println("This is selection sort");
+        int[] arr={5,4,3,2,1};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertion(int[] arr) {
+
+    }
 }
