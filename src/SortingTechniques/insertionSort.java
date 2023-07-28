1276f20d7f414285b5261c6cce4d4f62c0c0695d
Hardik Pratap <hardikpratap188@gmail.com>
2023-07-28 22:51:32 +0530

    swap function

--- src/SortingTechniques/insertionSort.java
+++ src/SortingTechniques/insertionSort.java
@@ -11,18 +11,20 @@ public class insertionSort {
     }
 
     private static void insertion(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j >0 ; j--) {
                 if(arr[j]<arr[j-1]){
-                    swap(arr[j],arr[j-1]);
+                    swap(arr,j,j-1);
                 }
                 else break;
             }
 
         }
     }
-    private static void swap(int first, int second) {
-
+    private static void swap(int[] arr,int first, int second) {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 
 
