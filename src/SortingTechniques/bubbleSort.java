e22b448426851ee2eec4552969f15cd6a4b565cc
Hardik Pratap <hardikpratap188@gmail.com>
2023-07-31 14:56:21 +0530

    bubble sort - full code

---
+++ src/SortingTechniques/bubbleSort.java
@@ -0,0 +1,30 @@
+package SortingTechniques;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        System.out.println("This is Bubble sort");
+        int[] arr={5,4,3,2,1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubble(int[] arr) {
+        boolean swap;
+        for (int i = 0; i < arr.length; i++) {
+            swap=false;
+            for (int j =1; j < arr.length ; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swap=true;
+                }
+
+            }
+            if (!swap) break;
+        }
+    }
+
+}
