ba7e615a36cadf64ad5cde08a75989d9cb2fca58
Hardik Pratap <hardikpratap188@gmail.com>
2023-07-31 14:57:31 +0530

    update bubble sort

--- src/SortingTechniques/bubbleSort.java
+++ src/SortingTechniques/bubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class bubbleSort {
     public static void main(String[] args) {
-        System.out.println("This is Bubble sort");
+        System.out.println("This is Bubble sorting");
         int[] arr={5,4,3,2,1};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
