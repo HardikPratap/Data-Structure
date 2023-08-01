2391bf9b7feae5f321d8abcf06eaf22405a3bc75
Hardik Pratap <hardikpratap188@gmail.com>
2023-08-01 14:18:40 +0530

    quick sort complete

--- src/SortingTechniques/quickSort.java
+++ src/SortingTechniques/quickSort.java
@@ -4,9 +4,36 @@ import java.util.Arrays;
 
 public class quickSort {
     public static void main(String[] args) {
-        System.out.println("This is Bubble sorting");
+        System.out.println("This is Quick sorting");
         int[] arr={5,4,3,2,1};
-        int[] ans=merge(arr);
-        System.out.println(Arrays.toString(ans));
+        quick(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quick(int[] arr,int low, int high) {
+        if(low>=high) return;
+
+        int s= low;
+        int e=high;
+        int mid= s+(e-s)/2;
+        int pivot= arr[mid];
+
+
+        while(s<=e){
+            while(arr[s]<pivot) s++;
+            while (arr[e]>pivot)e--;
+
+            if(s<=e){
+                int tepm= arr[s];
+                arr[s]=arr[e];
+                arr[e]=tepm;
+                s++;
+                e--;
+
+            }
+        }
+        quick(arr,low,e);
+        quick(arr,s,high);
+
     }
 }
